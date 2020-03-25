2b0afc59bc42d1962f4b04fca308c7d636afec49
jiho lee <jiholee@kpu.ac.kr>
2020-03-25 10:48:01 +0900

    ch1-11

---
+++ src/ch1/11/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "add.h"
+
+using namespace std;
+
+// 이번 강의의 주제
+
+// 헤더 파일 만들기
+
+
+int main(void) {
+    cout << add(1, 2) << endl;
+
+    return 0;
+}
+
+// gcc에서 main.cpp만 컴파일 시 undefined reference to `add(int, int)'
+// 해결 방안: g++ main.cpp add.cpp -g -Wall -W
+// 같이 컴파일한다.
